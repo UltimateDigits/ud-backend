007aaaa0a911a8a073a66fbd26d7e49cee529b4f
thirumurugan7 <thiru82003@gmail.com>
2024-03-19 20:40:27 +0530

    reverted changed

--- routes/coinbase.js
+++ routes/coinbase.js
@@ -8,14 +8,13 @@ const {
   getPhnoFromAddress,
 } = require("../controller/coinbase-auth");
 
-const { userLog } = require("../controller/coinbase-backend.ts");
 const router = express.Router();
 
 router.use(cors("*"));
 
 router.route("/verify").post(verifyUser);
 router.route("/map-phno").post(mapPhoneNumber);
-router.route("/coinbaseAuth").post(userLog);
+router.route("/coinbaseAuth").post(UserCoinbaseAuthToken);
 router.route("/getAddress").post(getAddressFromPhno);
 router.route("/getPhno").post(getPhnoFromAddress);
 
