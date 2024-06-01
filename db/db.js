f29bf49eb6cc2052fd9a622fc5bda6b5edb8e544
thirumurugan7 <thiru82003@gmail.com>
2024-06-02 02:24:18 +0530

    Changed db uri

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-const dbURI = "mongodb+srv://Thiru:Gryffindor7@cluster0.96vb1.mongodb.net/UD";
-
+const dbURI =
+  "mongodb+srv://udmain:Qm6CJDapfQ4ehxEF@ud-main.xnsjd.mongodb.net/?retryWrites=true&w=majority&appName=UD-main";
+//mongodb+srv://Thiru:Gryffindor7@cluster0.96vb1.mongodb.net/UD
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected successfully"))
