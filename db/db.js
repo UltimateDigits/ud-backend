98407b939994a1532245359a55c2a01c7f694cda
thirumurugan7 <thiru82003@gmail.com>
2024-07-02 12:19:15 +0530

    updated env

--- db/db.js
+++ db/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbURI =
-  "mongodb+srv://udmain:Qm6CJDapfQ4ehxEF@ud-main.xnsjd.mongodb.net/?retryWrites=true&w=majority&appName=UD-main";
-//mongodb+srv://Thiru:Gryffindor7@cluster0.96vb1.mongodb.net/UD
+const dbURI = process.env.DB;
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected successfully"))
