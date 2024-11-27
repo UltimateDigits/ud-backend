df53b856bf66a6b35a446fd24a4da8a7cd01f03a
thirumurugan7 <thiru82003@gmail.com>
2024-11-27 13:40:03 +0530

    sdfhsd

--- controller/winks-url.js
+++ controller/winks-url.js
@@ -1,5 +1,6 @@
 const Winks = require("../models/Winks");
 const { v4: uuidv4 } = require("uuid"); // Ensure you have this import for uuidv4
+const axios = require("axios");
 
 const createWink = async (req, res, next) => {
   console.log("called");
@@ -72,11 +73,25 @@ const getWink = async (req, res, next) => {
     res.status(404).json({ message: 'Link not found' });
   }
 };
+const proxyHer = async (req, res, next) => {
+
+  console.log("caleed proxy");
+  try {
+    const response = await axios.get("https://api.hercules.exchange/v2/tokens");
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(error.response ? error.response.status : 500)
+      .send("Error fetching data");
+  }
+
+};
 
 
 
 
 module.exports = {
   createWink,
-  getWink
+  getWink,
+  proxyHer
 };
--- routes/winks.js
+++ routes/winks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { createWink, getWink } = require("../controller/winks-url");
+const { createWink, getWink, proxyHer } = require("../controller/winks-url");
 
 const router = express.Router();
 router.use(cors("*"));
 
 router.route("/createWink").post(createWink);
 router.route("/getWink/:uniqueId").get(getWink);
+router.route("/proxy").get(proxyHer);
 
 module.exports = router;
